5266b38b8ca4a299efe42d084b3475f3772da8c8
greenpenguin <penguin.is.green@gmail.com>
2019-10-01 23:57:36 +0400

    no message

---
+++ DFA/DFA/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DFA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            WorkWithFileClass workWithFileClass = new WorkWithFileClass();
+            WorkWithConsoleClass workWithConsoleClass = new WorkWithConsoleClass();
+            DfaClass dfa = new DfaClass(workWithFileClass.ReadDfaInformationFromFile());
+            
+            workWithFileClass.PrintDfaToFile(dfa);
+
+            string inputStr = workWithConsoleClass.ConsoleInput();
+            dfa.OutputMax(inputStr);
+        }
+    }
+}
---
+++ DFA/DFA/WorkWithFileClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DFA
+{
+    public class WorkWithFileClass
+    {
+        private readonly string _inputFileName = Directory.GetCurrentDirectory() + "/input.txt";
+        private readonly string _outputFileName = Directory.GetCurrentDirectory() + "/output.txt";
+        
+        public List<string> ReadDfaInformationFromFile()
+        {
+            List<string> strFromFile;
+
+            using (StreamReader reader = new StreamReader(_inputFileName, true))
+            {
+                strFromFile = reader.ReadToEnd().Split('\n').ToList();
+            }
+
+            return strFromFile;
+        }
+
+        public void PrintDfaToFile(DfaClass dfa)
+        {
+            File.WriteAllText(_outputFileName, String.Empty);
+            using (StreamWriter writer = new StreamWriter(_outputFileName, true))
+            {
+                foreach (var elem in dfa.States)
+                {
+                    writer.Write(elem + " ");
+                }
+
+                writer.WriteLine();
+                
+                foreach (var elem in dfa.Alphabet)
+                {
+                    writer.Write(elem + " ");
+                }
+                
+                writer.WriteLine();
+                
+                foreach (var elem in dfa.TransitionFunction)
+                {
+                    writer.Write(elem.Key + " ");
+                    foreach (var elemValue in dfa.TransitionFunction[elem.Key])
+                    {
+                        writer.Write(elemValue + " ");
+                    }
+                    writer.WriteLine();
+                }
+                
+                foreach (var elem in dfa.StartStates)
+                {
+                    writer.Write(elem + " ");
+                }
+                
+                writer.WriteLine();
+                
+                foreach (var elem in dfa.FinalStates)
+                {
+                    writer.Write(elem + " ");
+                }
+            }
+        }
+        
+        
+    }
+}
